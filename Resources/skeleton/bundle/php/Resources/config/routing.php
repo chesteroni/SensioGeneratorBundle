e55aa1a70fbf1c334d259e7a4d36b5c58b1d64bb
chesteroni <chesteroni@gmail.com>
2011-06-25 10:06:09 -0700

    Enabled the default controller and template to work with URI schema: http://example.com/test

--- Resources/skeleton/bundle/php/Resources/config/routing.php
+++ Resources/skeleton/bundle/php/Resources/config/routing.php
@@ -4,7 +4,7 @@ use Symfony\Component\Routing\RouteCollection;
 use Symfony\Component\Routing\Route;
 
 $collection = new RouteCollection();
-$collection->add('{{ bundle }}_homepage', new Route('/', array(
+$collection->add('{{ bundle }}_homepage', new Route('/{name}', array(
     '_controller' => '{{ bundle }}:Default:index',
 )));
 
